22e1a422472928d88d18b8547f8175ca0b582d26
sebas123312231 <arceantezanasebastianalejandro@gmail.com>
2026-09-16 23:55:01 -0400

    feat(backend): activate rework application scan

--- backend/src/main/java/com/shopwavefusion/ShopwavefusionbackendApplication.java
+++ backend/src/main/java/com/shopwavefusion/ShopwavefusionbackendApplication.java
@@ -2,6 +2,9 @@ package com.shopwavefusion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
@@ -10,20 +13,15 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.security.SecuritySchemes;
 import io.swagger.v3.oas.annotations.servers.Server;
-@OpenAPIDefinition(info = @Info(title = "REST API", version = "1.1"),
-security = {
-	@SecurityRequirement(name = "basicAuth"), 
-	@SecurityRequirement(name = "bearerToken")
-	}, 
-servers = {
-	@Server(url = "/", description = "Default Server URL")
-    	}
-)
-@SecuritySchemes({
-@SecurityScheme(name = "basicAuth", type = SecuritySchemeType.HTTP, scheme = "basic"),
-@SecurityScheme(name = "bearerToken", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT")
-})
-@SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "ShopWave API", version = "1.0"),
+        security = @SecurityRequirement(name = "bearerToken"),
+        servers = @Server(url = "/", description = "Current server"))
+@SecuritySchemes(@SecurityScheme(name = "bearerToken", type = SecuritySchemeType.HTTP,
+        scheme = "bearer", bearerFormat = "JWT"))
+@SpringBootApplication(scanBasePackages = "com.shopwavefusion.rework")
+@EntityScan("com.shopwavefusion.rework.domain")
+@EnableJpaRepositories("com.shopwavefusion.rework.repository")
+@EnableMethodSecurity
 public class ShopwavefusionbackendApplication {
 
 	public static void main(String[] args) {
