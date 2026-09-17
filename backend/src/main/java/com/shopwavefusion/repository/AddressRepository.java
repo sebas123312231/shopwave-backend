16c3b0cd6b8aea3225837eb784c4a8a4eda75185
sebas123312231 <arceantezanasebastianalejandro@gmail.com>
2026-09-16 23:55:36 -0400

    refactor(backend): remove legacy repositories

--- backend/src/main/java/com/shopwavefusion/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.shopwavefusion.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.shopwavefusion.modal.Address;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-
-}
--- backend/src/main/java/com/shopwavefusion/repository/OrderItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.shopwavefusion.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.shopwavefusion.modal.OrderItem;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-
-}
